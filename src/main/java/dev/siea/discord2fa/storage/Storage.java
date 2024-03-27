dd2318a594d5a87252d604d00d00253853fb2993
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-03-27 01:30:41 +0100

    changed the file storage system cause ebic made me

---
+++ src/main/java/dev/siea/discord2fa/storage/Storage.java
@@ -0,0 +1,14 @@
+package dev.siea.discord2fa.storage;
+
+import dev.siea.discord2fa.storage.models.Account;
+
+import java.sql.SQLException;
+
+public interface Storage {
+    boolean isLinked(String uuid);
+    void linkAccount(String uuid, String discordID);
+    boolean isLinkedByDiscord(String discordID);
+    void unlinkAccount(String uuid);
+    Account findAccountByUUID(String uuid);
+    Account findAccountByDiscordID(String discordID);
+}
--- src/main/java/dev/siea/discord2fa/storage/StorageManager.java
+++ src/main/java/dev/siea/discord2fa/storage/StorageManager.java
@@ -1,8 +1,8 @@
 package dev.siea.discord2fa.storage;
 
-import dev.siea.discord2fa.Discord2FA;
-import dev.siea.discord2fa.storage.database.Database;
-import dev.siea.discord2fa.storage.file.FileUtil;
+import dev.siea.discord2fa.storage.file.FileStorage;
+import dev.siea.discord2fa.storage.mysql.MySQLStorage;
+import dev.siea.discord2fa.storage.mysql.MySQLWrapper;
 import dev.siea.discord2fa.storage.models.Account;
 import net.dv8tion.jda.api.entities.Member;
 import org.bukkit.entity.Player;
@@ -11,120 +11,52 @@ import org.bukkit.plugin.Plugin;
 import java.sql.SQLException;
 
 public class StorageManager {
-    private static StorageType storageType;
-    private static FileUtil fileUtil;
-
+    private static Storage accountStorage;
 
     public static void init(Plugin plugin) {
-        try{
-            storageType = StorageType.valueOf(plugin.getConfig().getString("storage"));
-        } catch (Exception e){
-            if (plugin.getConfig().getBoolean("fileAsFallback")){
-                storageType = StorageType.FILE;
-                plugin.getLogger().severe("Switching to File Storage(fileAsFallback) due to invalid Storage Type!");
+        try {
+            StorageType storageType = StorageType.valueOf(plugin.getConfig().getString("storage"));
+            switch (storageType) {
+                case MYSQL:
+                    MySQLWrapper.onEnable(plugin);
+                    accountStorage = new MySQLStorage();
+                    break;
+                case FILE:
+                    accountStorage = new FileStorage();
+                    break;
             }
-            else{
-                plugin.getLogger().severe(String.format("[%s] - Disabled due to invalid Storage Type! [THIS IS NOT A BUG DO NOT REPORT IT]", plugin.getDescription().getName()));
+        } catch (Exception e) {
+            if (plugin.getConfig().getBoolean("fileAsFallback")) {
+                plugin.getLogger().severe("Switching to File Storage (fileAsFallback) due to invalid Storage Type or connection failure!");
+                accountStorage = new FileStorage();
+            } else {
+                plugin.getLogger().severe(String.format("[%s] - Disabled due to invalid Storage Type or connection failure! [THIS IS NOT A BUG DO NOT REPORT IT]", plugin.getDescription().getName()));
                 plugin.getServer().getPluginManager().disablePlugin(plugin);
-                return;
             }
-        }
-
-        if (storageType == StorageType.MYSQL){
-            try {
-                Database.onEnable(plugin);
-            } catch (Exception e) {
-                if (plugin.getConfig().getBoolean("fileAsFallback")){
-                    storageType = StorageType.FILE;
-                    plugin.getLogger().severe("Switching to File Storage(fileAsFallback) due to being unable to connect to Database!");
-                }
-                else{
-                    plugin.getLogger().severe(String.format("[%s] - Disabled due to being unable to connect to Database! [THIS IS NOT A BUG DO NOT REPORT IT]", plugin.getDescription().getName()));
-                    plugin.getServer().getPluginManager().disablePlugin(plugin);
-                    return;
-                }
-            }
-        }
-        if (storageType == StorageType.FILE) {
-            fileUtil = new FileUtil();
         }
     }
 
     public static boolean isLinked(Player player) {
-        if (storageType == StorageType.MYSQL){
-            try {
-                return Database.findAccountByUUID(player.getUniqueId().toString()) != null;
-            } catch (SQLException e) {
-                return false;
-            }
-        }
-        else if (storageType == StorageType.FILE){
-            return fileUtil.findAccountByUUID(player.getUniqueId().toString())!= null;
-        }
-        return false;
+        return accountStorage.isLinked(player.getUniqueId().toString());
     }
 
     public static void linkAccount(Player player, String discordID) throws SQLException {
-        if (storageType == StorageType.MYSQL){
-            Database.createAccount(player.getUniqueId().toString(), discordID);
-        }
-        else if (storageType == StorageType.FILE){
-            fileUtil.createAccount(player.getUniqueId().toString(), discordID);
-        }
+        accountStorage.linkAccount(player.getUniqueId().toString(), discordID);
     }
 
     public static boolean isLinkedByDiscord(Member member) {
-        if (storageType == StorageType.MYSQL){
-            try {
-                return Database.findAccountByDiscordID(member.getId()) != null;
-            } catch (SQLException e) {
-                return false;
-            }
-        }
-        else if (storageType == StorageType.FILE){
-            fileUtil.findAccountByDiscordID(member.getId());
-        }
-        return false;
+        return accountStorage.isLinkedByDiscord(member.getId());
     }
 
     public static void unlinkAccount(Player player) {
-        if (storageType == StorageType.MYSQL){
-            try {
-                Database.deleteAccount(player.getUniqueId().toString());
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        else if (storageType == StorageType.FILE){
-            fileUtil.deleteAccount(player.getUniqueId().toString());
-        }
+        accountStorage.unlinkAccount(player.getUniqueId().toString());
     }
 
     public static Account findAccountByUUID(String uuid) {
-        if (storageType == StorageType.MYSQL){
-            try {
-                return Database.findAccountByUUID(uuid);
-            } catch (SQLException e) {
-                return null;
-            }
-        }
-        else if (storageType == StorageType.FILE){
-            return fileUtil.findAccountByUUID(uuid);
-        }
-        return null;
+        return accountStorage.findAccountByUUID(uuid);
     }
 
     public static Account findAccountByDiscordID(String id) {
-        if (storageType == StorageType.MYSQL){
-            try {
-                return Database.findAccountByDiscordID(id);
-            } catch (SQLException e) {
-                return null;
-            }
-        }
-        else if (storageType == StorageType.FILE){
-            return fileUtil.findAccountByDiscordID(id);
-        }
-        return null;
+        return accountStorage.findAccountByDiscordID(id);
     }
 }
