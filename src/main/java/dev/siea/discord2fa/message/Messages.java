77e5a2da43f77e90c9de5bfae4b0a3266c52f63a
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-05-13 18:40:14 +0200

    Language methods

--- src/main/java/dev/siea/discord2fa/message/Messages.java
+++ src/main/java/dev/siea/discord2fa/message/Messages.java
@@ -9,12 +9,20 @@ import java.util.HashMap;
 
 public class Messages {
     private static final HashMap<String, String> messages = new HashMap<>();
-    private static Plugin plugin;
-    private static final ConfigurationSection configUtil = new ConfigUtil(Discord2FA.getPlugin(), "messages.yml").getConfig();
+    private static ConfigurationSection configUtil = new ConfigUtil(Discord2FA.getPlugin(), "lang/en.yml").getConfig();
+    private static final ConfigurationSection backup = new ConfigUtil(Discord2FA.getPlugin(), "lang/messages.yml").getConfig();
 
-    public static void onEnable(Plugin plugin){
-        Messages.plugin = plugin;
+    public static void setLanguage(String lang){
+        Plugin plugin = Discord2FA.getPlugin();
+        try{
+            String path = "lang/" + lang.toLowerCase() + ".yml";
+            plugin.saveResource(path, false);
+            configUtil = new ConfigUtil(plugin, path).getConfig();
+        } catch (Exception ignore){
+
+        }
     }
+
     public static String get(String key) {
         return messages.computeIfAbsent(key, Messages::retrieveMessageFromConfig);
     }
@@ -22,7 +30,10 @@ public class Messages {
     private static String retrieveMessageFromConfig(String key) {
         String retrievedMessage = configUtil.getString(key);
         if (retrievedMessage == null) {
-            retrievedMessage = "§c§lThis is not a bug do not report it! §c[Discord2FA >> Config.yml] The following message is either missing or not set: §e" + key;
+            retrievedMessage = backup.getString(key);
+        }
+        if (retrievedMessage == null) {
+            retrievedMessage = "§c§lThis is not a bug do not report it! §c[Discord2FA >> Messages.yml] The following message is either missing or not set: §e" + key;
         }
         return retrievedMessage.replace("&","§");
     }
