aab7ab9729bfca65fc579e6638f8eae753e00071
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-05-16 23:57:00 +0200

    Fixed Update Checker

--- src/main/java/dev/siea/discord2fa/util/UpdateChecker.java
+++ src/main/java/dev/siea/discord2fa/util/UpdateChecker.java
@@ -13,28 +13,28 @@ public class UpdateChecker {
     private static final String RELEASES_URL = "https://api.github.com/repos/sieadev/discord2fa/releases";
 
     public UpdateChecker(Plugin plugin) {
-        String currentVersion = plugin.getDescription().getVersion();
+        String currentVersion = "V" + plugin.getDescription().getVersion();
         int releasesBehind = getReleasesBehind(currentVersion);
         if (releasesBehind > 0) {
-            plugin.getLogger().severe("You are " + releasesBehind + " releases behind! Download it at https://modrinth.com/plugin/discord2fa");
+            plugin.getLogger().severe("You are " + releasesBehind + " release(s) behind! Download the newest release at https://modrinth.com/plugin/discord2fa");
         }
     }
 
     private int getReleasesBehind(String currentVersion) {
         try {
+            int behind = 0;
             JsonArray releases = getAllReleases();
             if (releases != null) {
                 for (JsonElement release : releases) {
                     String tagName = release.getAsJsonObject().get("tag_name").getAsString();
-                    if (tagName.equalsIgnoreCase(currentVersion)) {
-                        return 0;
-                    }
+                    if (!tagName.equalsIgnoreCase(currentVersion)) behind++;
+                    else return behind;
                 }
-                return releases.size();
+                return behind;
             }
         } catch (IOException ignore) {
         }
-        return -1;
+        return 0;
     }
 
     private JsonArray getAllReleases() throws IOException {
