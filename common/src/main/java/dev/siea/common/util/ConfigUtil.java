35bf47316d80070721cb36d48003a6495dee1b69
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-05-22 22:52:08 +0200

    c

--- common/src/main/java/dev/siea/common/util/ConfigUtil.java
+++ common/src/main/java/dev/siea/common/util/ConfigUtil.java
@@ -6,9 +6,9 @@ import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
 import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
+import dev.siea.common.Common;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -17,6 +17,7 @@ public class ConfigUtil {
     private YamlDocument config;
 
     public ConfigUtil(Path dir, String path) {
+        System.out.println("Test");
         try {
             config = YamlDocument.create(new File(dir.toFile(), path),
                     Objects.requireNonNull(getClass().getResourceAsStream("/" + path)),
@@ -28,7 +29,8 @@ public class ConfigUtil {
 
             config.update();
             config.save();
-        } catch (IOException ignore) {
+        } catch (Exception ignore) {
+            Common.getInstance().log("Failed to load config file: " + path);
         }
         this.file = new File(path);
     }
